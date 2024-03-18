a0a129c49bddafb2aab1fa8bce3aeec26cc5273b
Rehan <rehanalire52@gmail.com>
2024-03-18 21:08:58 +0530

    setup backend and connect it to database

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,11 +1,10 @@
-import  Express  from "express";
+import Express from "express";
 import mongoose from "mongoose";
 
 const Server = Express();
 const port = 3000;
 
-const uri = 'mongodb+srv://<rehan>:<G0LTj1GIvPrhAoLJ>@rehan.cycatda.mongodb.net/?retryWrites=true&w=majority&appName=Rehan';
-
+const uri = 'mongodb+srv://Blog_Data:rehan12345@rehan.cycatda.mongodb.net/?retryWrites=true&w=majority&appName=Rehan';
 
 const dataSchema = new mongoose.Schema({
     name: String,
@@ -14,35 +13,30 @@ const dataSchema = new mongoose.Schema({
 
 const dataModel = mongoose.model("usermodel", dataSchema);
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(uri)
 .then(() => {
-   console.log('Db connected');
-   const dataSample = new dataModel({name: 'Rehan', age: 18});
-   dataSample.save()
-   .then(() => {
-    console.log(' data inserted');
-    dataModel.find({}, (err, data) => {
-      if (err) {
-        console.error('Error querying database:', err);
-        return;
-      }
-      console.log('Sample data retrieved:', data);
+    console.log('Db connected');
+    const dataSample = new dataModel({name: 'Rehan', age: 18});
+    dataSample.save()
+    .then(() => {
+        console.log('data inserted');
+        return dataModel.find({}); 
+    })
+    .then((data) => { 
+        console.log('Sample data retrieved:', data);
+    })
+    .catch((err) => {
+        console.error('Error retrieving sample data:', err);
     });
-  })
-  .catch((err) => {
-    console.error('Error inserting sample data:', err);
-  });
 })
 .catch((err) => {
-  console.log("Error", err);
-})
-
+    console.error("Error connecting to database:", err);
+});
 
 Server.get('/', (req, res) => {
     res.send('Hello');
-})
-
+});
 
 Server.listen(port, () => {
     console.log(`Server is running on ${port}`);
-})
+});
